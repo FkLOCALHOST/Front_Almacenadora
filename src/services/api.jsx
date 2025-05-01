25cd8ce51c989c80bac83dff207733c75e20b9cf
unknown <dbercian-2020527@kinal.edu.gt>
2025-04-30 20:41:27 -0600

    fix(Productos): se arregló el listar de productos

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -159,7 +159,7 @@ export const agregarProducto = async (data) => {
 
 export const listarProductos = async () => {
   try {
-    const response = await apiAlmacenadora.get("/productos/listarProductos"); // Corrected endpoint
+    const response = await apiAlmacenadora.get("productos/listarProductos"); // Corrected endpoint
     return response; // Ensure response.data contains the product list
   } catch (error) {
     return { error: true, message: error.message };
