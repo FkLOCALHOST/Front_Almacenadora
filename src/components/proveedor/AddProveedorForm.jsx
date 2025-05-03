8386e59ac32aa3d47910d92282e466b666147da8
¨Dgarcia-09¨ <dgarcia-2023532@kinal.edu.gt>
2025-05-03 12:36:24 -0600

    fix(Proveedores): coreccion de errores en merge

--- src/components/proveedor/AddProveedorForm.jsx
+++ src/components/proveedor/AddProveedorForm.jsx
@@ -1,137 +1,46 @@
-import React, {useState, useEffect} from 'react';
-import PropTypes from 'prop-types';
-import { agregarProveedor, actualizarProveedor } from '../../services/api';
-import './AddProveedorForm.css';
-
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { agregarProveedor, actualizarProveedor } from "../../services/api";
+import "./AddProveedorForm.css";
 
 const AddProveedorForm = ({
-    onClose,
-    onSubmit,
-    initialData = null,
-    isAdmin
+  onClose,
+  onSubmit,
+  initialData = null,
+  isAdmin,
+  fetchProveedores,
 }) => {
-<<<<<<< Updated upstream
-    const [formData, setFormData] = useState({
-        nombre: "",
-        direccion: "",
-        telefono: "",
-        estado: true,
-    });
-=======
   const [formData, setFormData] = useState({
     nombre: "",
     direccion: "",
     telefono: "",
   });
-  const [error, setError] = useState(""); 
->>>>>>> Stashed changes
+  const [errorMessage, setErrorMessage] = useState(""); // Estado para el mensaje de error
 
-    useEffect(() => {
-        if(initialData){
-            setFormData({
-                nombre: initialData.nombre || "",
-                direccion: initialData.direccion || "",
-                telefono: initialData.telefono || "",
-                estado: initialData.estado !== undefined ? initialData.estado : true,
-            });
-        }
-    }, [initialData]);
+  useEffect(() => {
+    if (initialData) {
+      setFormData({
+        nombre: initialData.nombre || "",
+        direccion: initialData.direccion || "",
+        telefono: initialData.telefono || "",
+      });
+    }
+  }, [initialData]);
 
-    const handleChange =  (e) => {
-        const  { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-<<<<<<< Updated upstream
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-        const telefonoNumerico = Number(formData.telefono);
-        if (isNaN(telefonoNumerico)) {
-            alert("El teléfono debe contener solo números.");
-            return;
-        }
-    
-        try {
-            let response;
-            const dataToSend = {
-                ...formData,
-                telefono: telefonoNumerico,
-            };
-    
-            if (initialData && initialData._id) {
-                response = await actualizarProveedor(initialData._id, dataToSend);
-            } else {
-                response = await agregarProveedor(dataToSend);
-            }
-    
-            if (!response.error) {
-                onSubmit(response.data); 
-                onClose();
-            } else {
-                console.error('Error al procesar la solicitud:', response.message);
-            }
-    
-        } catch (error) {
-            console.error('Error al conectar con el servidor: ', error);
-        }
-    };
-
-return (
-    <div className="add-proveedor-form-container">
-        <div className="add-proveedor-form-content">
-            <h2 className="form-title">{initialData ? 'Actualizar Proveedor' : 'Agregar Proveedor'}</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="input-group">
-                    <input
-                        type="text"
-                        name="nombre"
-                        value={formData.nombre}
-                        onChange={handleChange}
-                        placeholder="Nombre del Proveedor"
-                        required
-                    />
-                </div>
-                <div className="input-group">
-                    <input
-                        type="text"
-                        name="direccion"
-                        value={formData.direccion}
-                        onChange={handleChange}
-                        placeholder="Dirección del Proveedor"
-                        required
-                    />
-                </div>
-                <div className="input-group">
-                    <input
-                        type="text"
-                        name="telefono"
-                        value={formData.telefono}
-                        onChange={handleChange}
-                        placeholder="Teléfono del Proveedor"
-                        required
-                    />
-                </div>
-                {isAdmin &&(
-                    <button type="submit" className="form-button">
-                        {initialData ? 'Actualizar' : 'Agregar'}
-                    </button>
-                )}
-                <button type="button" className="form-button cancel-button" onClick={onClose}>
-                    Cancelar
-                </button>
-=======
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError(""); 
 
     const telefonoNumerico = Number(formData.telefono);
     if (isNaN(telefonoNumerico)) {
-      setError("El teléfono debe contener solo números.");
+      setErrorMessage("El teléfono debe contener solo números.");
       return;
     }
 
@@ -153,13 +62,12 @@ return (
         await fetchProveedores();
         onSubmit(response.data);
         onClose();
+        setErrorMessage(""); // Limpiar el mensaje de error si la solicitud es exitosa
       } else {
-        setError(response.message || "Error al procesar la solicitud.");
-        console.error("Error al procesar la solicitud:", response.message);
+        setErrorMessage(response.message || "Error al procesar la solicitud.");
       }
     } catch (error) {
-      setError("Error al conectar con el servidor.");
-      console.error("Error al conectar con el servidor: ", error);
+      setErrorMessage("Error al conectar con el servidor: " + error.message);
     }
   };
 
@@ -169,6 +77,7 @@ return (
         <h2 className="form-title">
           {initialData ? "Actualizar Proveedor" : "Agregar Proveedor"}
         </h2>
+        {errorMessage && <div className="error-message">{errorMessage}</div>} {/* Mostrar el mensaje de error */}
         <form onSubmit={handleSubmit}>
           <div className="input-group">
             <input
@@ -200,10 +109,6 @@ return (
               required
             />
           </div>
-
-          {/* Mostrar error si ocurre alguno */}
-          {error && <div className="error-message">{error}</div>}
-
           {isAdmin && (
             <button type="submit" className="form-button">
               {initialData ? "Actualizar" : "Agregar"}
@@ -221,20 +126,13 @@ return (
     </div>
   );
 };
->>>>>>> Stashed changes
 
-            </form>
-       
-        </div>
-    </div>
-)
-}
-
-AddProveedorForm.propTypes ={
-    onClose: PropTypes.func.isRequired,
-    onSubmit: PropTypes.func.isRequired,
-    initialData: PropTypes.object,
-    isAdmin: PropTypes.bool.isRequired,
-}
+AddProveedorForm.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  initialData: PropTypes.object,
+  isAdmin: PropTypes.bool.isRequired,
+  fetchProveedores: PropTypes.func.isRequired,
+};
 
 export default AddProveedorForm;
